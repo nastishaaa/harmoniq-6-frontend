433af21c3a5ff8bf8f10d3b69f37832b87f0b6b3
Tetiana Hutorova <tttanchosss@gmail.com>
2025-07-25 21:47:38 +0200

    feat: create feature/articles-page branch

--- src/pages/ArticlesPage/ArticlesPage.jsx
+++ src/pages/ArticlesPage/ArticlesPage.jsx
@@ -1,7 +1,22 @@
-// import styles from './ArticlesPage.module.css';
+// import styles from "./ArticlesPage.module.css";
 
+import { useEffect } from "react";
 export default function ArticlesPage() {
-    return (
-        <div>ArticlesPage</div>
-    )
+  const N = Math.round(Math.random() * 10);
+  useEffect(() => {
+    console.log(5);
+  }, []);
+  return (
+    <div>
+       <div>
+         <h1>Articles</h1> <p>{N > 1 ? ` ${N} articles` : `${N} article`} </p> 
+      </div>
+      <div>
+         <select name="filter" id="choice">
+           <option value="all">All</option> 
+          <option value="popular">Popular</option> 
+        </select>{" "}
+      </div>{" "}
+    </div>
+  );
 }
