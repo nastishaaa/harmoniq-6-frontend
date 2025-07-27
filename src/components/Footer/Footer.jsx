3b8fc2ddb41b8ce08c578ffd24bfaed68be2232b
ash <a.motelchuk@gmail.com>
2025-07-27 16:22:40 +0300

    header unauth and nav

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,20 @@
-import styles from './Footer.module.css';
+import styles from "./Footer.module.css";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
-    return (
-        <div>Footer</div>
-    )
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <img src="/logo.png" alt="Harmoniq logo" />
+        </div>
+
+        <div className={styles.links}>
+          <Link to="/articles">Articles</Link>
+        </div>
+
+        <div className={styles.copy}>© 2025 Harmoniq. All rights reserved.</div>
+      </div>
+    </footer>
+  );
 }
