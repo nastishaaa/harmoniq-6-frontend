6191ffd1a859557791d6c23d88fdca345f070f91
Tetiana Hutorova <tttanchosss@gmail.com>
2025-07-28 01:38:07 +0200

    install react-select

--- src/pages/ArticlesPage/ArticlesPage.jsx
+++ src/pages/ArticlesPage/ArticlesPage.jsx
@@ -1,7 +1,9 @@
 import styles from "./ArticlesPage.module.css";
 import SectionTitle from "../../components/SectionTitle/SectionTitle";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import Select from "react-select";
+
 
 import {
   selectArticles,
@@ -14,6 +16,43 @@ export default function ArticlesPage() {
   const articlesItems = useSelector(selectArticles);
   const isLoading = useSelector(selectLoading);
   const isError = useSelector(selectError);
+
+    const [selectedOption, setSelectedOption] = useState({
+      value: "popular",
+      label: "Popular",
+    });
+    const options = [
+      { value: "all", label: "All" },
+      { value: "popular", label: "Popular" },
+    ];
+    const customStyles = {
+      control: (base) => ({
+        ...base,
+        width: "169px",
+        padding: "4px 8px",
+        fontSize: "16px",
+        borderRadius: "8px",
+        borderColor: "#9f9f9f",
+        boxShadow: "none",
+        cursor: "pointer",
+ 
+      }),
+      option: (base, { isFocused, isSelected }) => ({
+        ...base,
+        backgroundColor: isSelected
+          ? "#649179"
+          : isFocused
+          ? "#eef4f0"
+          : "white",
+        color: "#333",
+        cursor: "pointer",
+      }),
+      singleValue: (base) => ({
+        ...base,
+        color: "#595d62",
+      }),
+    };
+
 
   useEffect(() => {
     console.log("Юз eфект спрацював");
@@ -30,10 +69,19 @@ export default function ArticlesPage() {
             ? ` ${articlesItemsLength} articles`
             : `${articlesItemsLength} article`}
         </p>
-        <select name="filter" id="filter" className={styles.filter}>
+        {/* <select name="filter" id="filter" className={styles.filter}>
           <option value="all">All</option>
           <option value="popular">Popular</option>
-        </select>{" "}
+        </select>{" "} */}
+        <Select
+          value={selectedOption}
+          onChange={setSelectedOption}
+          options={options}
+          styles={customStyles}
+          className={styles.filterSelect}
+          isSearchable={false}
+          
+        />
       </div>{" "}
       {articlesItemsLength > 0 && <ArticlesList />}
       {isLoading && <p>Почекайте</p>}
