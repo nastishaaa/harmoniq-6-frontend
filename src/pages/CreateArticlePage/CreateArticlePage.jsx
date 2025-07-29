160b006febba29a2638be7d8003fb625bdb42002
HnatenkoOlya <gnatenkoolgaig1210@gmail.com>
2025-07-29 12:36:47 +0200

    addArticleForm

--- src/pages/CreateArticlePage/CreateArticlePage.jsx
+++ src/pages/CreateArticlePage/CreateArticlePage.jsx
@@ -1,7 +1,27 @@
-import styles from './CreateArticlePage.module.css';
+import AddArticleForm from "../../components/AddArticleForm/AddArticleForm";
+import { useSelector } from "react-redux";
+//import { useEffect } from "react";
+import { selectLoading, selectError, selectIsLoggedIn} from '../../redux/articles/selectors.js';
+//import {addArticle} from '../../redux/addArticles/addArticlesOperations.js';
+import css from './CreateArticlePage.module.css';
 
 export default function CreateArticlePage() {
+  //const dispatch = useDispatch();
+  const loading = useSelector(selectLoading);
+  const error = useSelector(selectError);
+const isLoggedIn = useSelector(selectIsLoggedIn);
+
+if (!isLoggedIn) {
+    return <Navigate to="/login" />;
+  }
+
+
     return (
-        <div>CreateArticlePage</div>
+        <div className={css.form}>
+        <h1>Create an article</h1>
+        {loading && <p>Loading ...</p>}
+      {error && <p>{error}</p>}
+      <AddArticleForm/>
+        </div>
     )
 }
