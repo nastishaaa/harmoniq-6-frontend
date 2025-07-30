b63d9dbb1caad91554d21b050c2e4e0c434ac286
Danyil Shevchenko <liberlay555@gmail.com>
2025-07-30 03:45:19 +0300

    Create reducer&slice for global flags

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
-import articlesReducer from "./articles/slice";
+import { configureStore } from '@reduxjs/toolkit'
+import globalReducer from './global/slice'
+import articlesReducer from './articles/slice'
 
 export const store = configureStore({
   reducer: {
+    global: globalReducer,
     articles: articlesReducer,
   },
- });
+})
