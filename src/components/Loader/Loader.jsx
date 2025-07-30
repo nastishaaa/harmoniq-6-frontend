65bac7c64af5d5b26e821b0d997a936242ead5b7
Anastasiia <anastasiiprfnk@gmail.com>
2025-07-30 17:52:45 +0300

    new spinner library

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx'
-import { Rings } from 'react-loader-spinner'
+import { BeatLoader } from 'react-spinners'
 
 import styles from './Loader.module.css'
 
@@ -7,7 +7,7 @@ export const Loader = () => {
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <Rings ariaLabel="loading-spinner" wrapperClass="wrapper" color="#649179" />
+        <BeatLoader color="#374f42a4" />
         <p className={clsx(styles.caption, styles.shinyText)} style={{ animationDuration: '3s' }}>
           Loading, please wait...
         </p>
