9c56994fd50926ca3a6977cbd0dfd16fad96e61d
Elizabeth Dzhuravel <eliz5678@gmail.com>
2025-07-31 16:56:05 +0300

    Added global modal window connection for unauthorized saving

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,64 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { lazy } from 'react'
-import RootLayout from './layout/RootLayout'
-import { useSelector } from 'react-redux'
-import { Loader } from './components/Loader/Loader.jsx'
-import { isLoading } from './redux/global/selectors.js'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { lazy } from "react";
+import RootLayout from "./layout/RootLayout";
+import { useSelector } from "react-redux";
+import { Loader } from "./components/Loader/Loader.jsx";
+import { isLoading } from "./redux/global/selectors.js";
+import { ModalErrorSave } from "./components/ModalErrorSave/ModalErrorSave.jsx";
 
-const HomePage = lazy(() => import('./pages/HomePage/HomePage.jsx'))
-const ArticlesPage = lazy(() => import('./pages/ArticlesPage/ArticlesPage.jsx'))
-const ArticleDetailPage = lazy(() => import('./pages/ArticleDetailPage/ArticleDetailPage.jsx'))
-const AuthorsPage = lazy(() => import('./pages/AuthorsPage/AuthorsPage.jsx'))
-const RegisterPage = lazy(() => import('./pages/RegisterPage/RegisterPage.jsx'))
-const UploadPhotoPage = lazy(() => import('./pages/UploadPhotoPage/UploadPhotoPage.jsx'))
-const LoginPage = lazy(() => import('./pages/LoginPage/LoginPage.jsx'))
-const CreatorsPage = lazy(() => import('./pages/CreatorsPage/CreatorsPage.jsx'))
-const CreatorDetailPage = lazy(() => import('./pages/CreatorDetailPage/CreatorDetailPage.jsx'))
-const CreateArticlePage = lazy(() => import('./pages/CreateArticlePage/CreateArticlePage.jsx'))
+const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
+const ArticlesPage = lazy(() =>
+  import("./pages/ArticlesPage/ArticlesPage.jsx")
+);
+const ArticleDetailPage = lazy(() =>
+  import("./pages/ArticleDetailPage/ArticleDetailPage.jsx")
+);
+const AuthorsPage = lazy(() => import("./pages/AuthorsPage/AuthorsPage.jsx"));
+const RegisterPage = lazy(() =>
+  import("./pages/RegisterPage/RegisterPage.jsx")
+);
+const UploadPhotoPage = lazy(() =>
+  import("./pages/UploadPhotoPage/UploadPhotoPage.jsx")
+);
+const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage.jsx"));
+const CreatorsPage = lazy(() =>
+  import("./pages/CreatorsPage/CreatorsPage.jsx")
+);
+const CreatorDetailPage = lazy(() =>
+  import("./pages/CreatorDetailPage/CreatorDetailPage.jsx")
+);
+const CreateArticlePage = lazy(() =>
+  import("./pages/CreateArticlePage/CreateArticlePage.jsx")
+);
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <RootLayout />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: 'articles', element: <ArticlesPage /> },
-      { path: 'articles/:id', element: <ArticleDetailPage /> },
-      { path: 'authors', element: <AuthorsPage /> },
-      { path: 'register', element: <RegisterPage /> },
-      { path: 'photo', element: <UploadPhotoPage /> },
-      { path: 'login', element: <LoginPage /> },
-      { path: 'authors', element: <CreatorsPage /> },
-      { path: 'authors/:id', element: <CreatorDetailPage /> },
-      { path: 'create', element: <CreateArticlePage /> },
+      { path: "articles", element: <ArticlesPage /> },
+      { path: "articles/:id", element: <ArticleDetailPage /> },
+      { path: "authors", element: <AuthorsPage /> },
+      { path: "register", element: <RegisterPage /> },
+      { path: "photo", element: <UploadPhotoPage /> },
+      { path: "login", element: <LoginPage /> },
+      { path: "authors", element: <CreatorsPage /> },
+      { path: "authors/:id", element: <CreatorDetailPage /> },
+      { path: "create", element: <CreateArticlePage /> },
     ],
   },
-])
+]);
 
 function App() {
-  const isGlobalLoading = useSelector(isLoading)
+  const isGlobalLoading = useSelector(isLoading);
   return (
     <>
       {isGlobalLoading && <Loader />}
       <RouterProvider router={router} />
+      <ModalErrorSave />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/ModalErrorSave/ModalErrorSave.jsx
+++ src/components/ModalErrorSave/ModalErrorSave.jsx
@@ -1,56 +1,72 @@
-import clsx from 'clsx'
-import { useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { createPortal } from 'react-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { setIsModalErrorSaveOpen } from '../../redux/global/slice'
-import { isModalErrorSaveOpen } from '../../redux/global/selectors'
+import clsx from "clsx";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { createPortal } from "react-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { setIsModalErrorSaveOpen } from "../../redux/global/slice";
+import { isModalErrorSaveOpen } from "../../redux/global/selectors";
 
-import styles from './ModalErrorSave.module.css'
+import styles from "./ModalErrorSave.module.css";
 
 export const ModalErrorSave = () => {
-  const dispatch = useDispatch()
-  const isModalOpen = useSelector(isModalErrorSaveOpen)
+  const dispatch = useDispatch();
+  const isModalOpen = useSelector(isModalErrorSaveOpen);
 
-  const bodySelector = document.querySelector('body')
+  const bodySelector = document.querySelector("body");
 
-  isModalOpen ? (bodySelector.style.overflow = 'hidden') : (bodySelector.style.overflow = 'auto')
+  isModalOpen
+    ? (bodySelector.style.overflow = "hidden")
+    : (bodySelector.style.overflow = "auto");
 
   useEffect(() => {
     const handleKeyDown = (e) => {
-      if (e.key === 'Escape') {
-        dispatch(setIsModalErrorSaveOpen(false))
+      if (e.key === "Escape") {
+        dispatch(setIsModalErrorSaveOpen(false));
       }
-    }
+    };
 
-    document.addEventListener('keydown', handleKeyDown)
+    document.addEventListener("keydown", handleKeyDown);
     return () => {
-      dispatch(setIsModalErrorSaveOpen(false))
-      document.removeEventListener('keydown', handleKeyDown)
-    }
-  }, [dispatch])
+      dispatch(setIsModalErrorSaveOpen(false));
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [dispatch]);
 
   return createPortal(
     <div
       className={clsx(styles.wrapper, isModalOpen && styles.wrapperOpen)}
       onClick={() => dispatch(setIsModalErrorSaveOpen(false))}
     >
-      <div className={clsx(styles.modal, isModalOpen && styles.modalOpen)} onClick={(e) => e.stopPropagation()}>
-        <button className={styles.closeButton} onClick={() => dispatch(setIsModalErrorSaveOpen(false))}>
+      <div
+        className={clsx(styles.modal, isModalOpen && styles.modalOpen)}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <button
+          className={styles.closeButton}
+          onClick={() => dispatch(setIsModalErrorSaveOpen(false))}
+        >
           <img src="src/assets/icons/close.svg" alt="Close icon" />
         </button>
         <p className={styles.title}>Error while saving</p>
-        <p className={styles.caption}>To save this article, you need to authorize first</p>
+        <p className={styles.caption}>
+          To save this article, you need to authorize first
+        </p>
         <div className={styles.buttonsContainer}>
-          <Link to={'/login'} className={clsx(styles.button, styles.buttonLogin)}>
+          <Link
+            to={"/login"}
+            className={clsx(styles.button, styles.buttonLogin)}
+          >
             Login
           </Link>
-          <Link to={'/register'} className={clsx(styles.button, styles.buttonRegister)}>
+          <Link
+            to={"/register"}
+            className={clsx(styles.button, styles.buttonRegister)}
+          >
             Register
           </Link>
         </div>
       </div>
     </div>,
-    document.getElementById('modals')
-  )
-}
+    document.getElementById("modals")
+  );
+};
