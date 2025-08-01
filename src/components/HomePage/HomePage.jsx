7507c0b6b09874296f7a63bb85fd384bd4f1a0cd
Maryna <89296066105marina@gmal.com>
2025-08-01 17:37:31 +0200

    HomePage-NotAuth

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,9 +1,25 @@
-const HomePage = () => {
+import About from "../About/About.jsx";
+import Header from "../Header/Header.jsx";
+import Hero from "../Hero/Hero.jsx";
+import PopularArticles from "../PopularArticles/PopularArticles.jsx";
+import Footer from "../Footer/Footer.jsx";
+import CreateArticlePage from "../../pages/CreateArticlePage/CreateArticlePage.jsx";
+
+const Home = () => {
   return (
     <>
-      
+      <Header />
+
+      <main>
+        <Hero />
+        <About />
+        <PopularArticles />
+        {/* <CreateArticlePage/> */}
+      </main>
+
+      {/* <Footer /> */}
     </>
   );
 };
 
-export default HomePage;
+export default Home;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,24 +1,30 @@
-import styles from './HomePage.module.css';
-import Hero from '../../components/Hero/Hero';
-import About from '../../components/About/About';
-import { useSelector, useDispatch } from 'react-redux';
-import { selectIsError, selectIsLoading } from '../../redux/homeData/selectors';
-import { useEffect } from 'react';
-import { fetchHomeArticles } from '../../redux/homeData/operations';
-import { Loader } from '../../components/Loader/Loader';
-import PopularArticles from '../../components/PopularArticles/PopularArticles';
+import styles from "./HomePage.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { selectIsError, selectIsLoading } from "../../redux/homeData/selectors";
+import { useEffect } from "react";
+import { fetchHomeArticles } from "../../redux/homeData/operations";
+import { Loader } from "../../components/Loader/Loader";
+import { selectIsLoggedIn } from "../../redux/register/selector.js";
+import HomeAuthorised from "../../components/HomePage/HomeAuthorised.jsx";
+import Home from "../../components/HomePage/HomePage.jsx";
 
 export default function HomePage() {
-    const dispatch = useDispatch();
-    useEffect(() => { dispatch(fetchHomeArticles()) }, [dispatch]);
-    const isLoading = useSelector(selectIsLoading);
-    const isError = useSelector(selectIsError);
-    return (
-        isLoading ? <Loader /> :
-        <div className={styles.container}>
-            <Hero />
-            <About />
-            <PopularArticles />
-        </div>
-    )
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchHomeArticles());
+  }, [dispatch]);
+  const isLoading = useSelector(selectIsLoading);
+  const isError = useSelector(selectIsError);
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  if (isError) return <div>Error</div>;
+  if (isLoading) return <Loader />;
+
+  return isLoggedIn ? (
+    <HomeAuthorised />
+  ) : (
+    <div className={styles.container}>
+      <Home />
+    </div> // треба додати Creators
+  );
 }
