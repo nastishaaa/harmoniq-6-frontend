6709068eba5e9243105f28f263ff7fc222c7bb06
yura6669 <yurii.shyian.01@gmail.com>
2025-08-04 14:34:06 +0300

    Fix home top creators

--- src/components/AuthorsItem/AuthorsItem.jsx
+++ src/components/AuthorsItem/AuthorsItem.jsx
@@ -2,18 +2,23 @@ import { useNavigate } from 'react-router-dom'
 
 import styles from './AuthorsItem.module.css'
 
-const AuthorsItem = ({ author, ref }) => {
+const AuthorsItem = ({ author, ref, isShowOnlyName = false }) => {
   const navigate = useNavigate()
 
   const handleClick = () => {
     navigate(`/users/${author._id}`)
   }
 
+  let name = author.name;
+  if (isShowOnlyName) { 
+    name = author.name.split(' ')[0] // Show only the first name
+  }
+
   return (
     <li className={styles.authorsItem} ref={ref}>
       <div style={{ cursor: 'pointer' }} onClick={handleClick}>
         <img src={author.avatarUrl} alt={author.name} className={styles.authorImage} />
-        <p className={styles.authorName}>{author.name}</p>
+        <p className={styles.authorName}>{name}</p>
       </div>
     </li>
   )
