7ff56ff11bbac46711833e2a550969fdad3cc502
Danyil Shevchenko <liberlay555@gmail.com>
2025-08-06 01:30:44 +0300

    Add Pagination to ArticlesPage & change redux slice logic

--- src/redux/articles/slice.js
+++ src/redux/articles/slice.js
@@ -1,6 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { fetchArticleById, fetchArticles } from "./operations";
-import { addArticle } from "../addArticle/addArticleOperations";
+import { createSlice } from '@reduxjs/toolkit'
+import { fetchArticleById, fetchArticles } from './operations'
+import { addArticle } from '../addArticle/addArticleOperations'
 
 const initialState = {
   items: [],
@@ -12,57 +12,57 @@ const initialState = {
   isErrorArticles: false,
   isLoadingArticle: false,
   isErrorArticle: false,
-};
+}
 
 const slice = createSlice({
-  name: "articles",
+  name: 'articles',
   initialState,
   extraReducers: (builder) => {
     builder
       .addCase(fetchArticles.pending, (state) => {
-        state.isLoadingArticles = true;
-        state.isErrorArticles = false;
+        state.isLoadingArticles = true
+        state.isErrorArticles = false
       })
       .addCase(fetchArticles.fulfilled, (state, action) => {
-        state.isLoadingArticles = false;
-        state.total = action.payload.total;
-        state.hasNextPage = action.payload.hasNextPage;
-        state.page = action.payload.page;
-        if (action.payload.page === 1) {
-          state.items = action.payload.data;
-        } else {
-          state.items = [...state.items, ...action.payload.data];
-        }
+        state.isLoadingArticles = false
+        state.total = action.payload.total
+        state.hasNextPage = action.payload.hasNextPage
+        state.page = action.payload.page
+        state.items = action.payload.data
+        // if (action.payload.page === 1) {
+        //   state.items = action.payload.data;
+        // } else {
+        //   state.items = [...state.items, ...action.payload.data];
+        // }
       })
       .addCase(fetchArticles.rejected, (state) => {
-        state.isLoadingArticles = false;
-        state.isErrorArticles = true;
+        state.isLoadingArticles = false
+        state.isErrorArticles = true
       })
       .addCase(fetchArticleById.pending, (state) => {
-        state.isLoadingArticle = true;
-        state.isErrorArticle = false;
+        state.isLoadingArticle = true
+        state.isErrorArticle = false
       })
       .addCase(fetchArticleById.fulfilled, (state, action) => {
-        state.isLoadingArticle = false;
-        state.selectedArticle = action.payload.data;
+        state.isLoadingArticle = false
+        state.selectedArticle = action.payload.data
       })
       .addCase(fetchArticleById.rejected, (state) => {
-        state.selectedArticle = null;
-        state.isLoadingArticle = false;
-        state.isErrorArticle = true;
+        state.selectedArticle = null
+        state.isLoadingArticle = false
+        state.isErrorArticle = true
       })
       .addCase(addArticle.fulfilled, (state, action) => {
-  const newArticle = action.payload;
+        const newArticle = action.payload
 
-  if (state.page === 1) {
-    state.items.unshift(newArticle);
-    state.total += 1;
-  } else {
-    state.total += 1;
-  }
-});
-
+        if (state.page === 1) {
+          state.items.unshift(newArticle)
+          state.total += 1
+        } else {
+          state.total += 1
+        }
+      })
   },
-});
+})
 
-export default slice.reducer;
+export default slice.reducer
